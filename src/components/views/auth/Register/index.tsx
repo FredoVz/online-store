cc6ced9bbd3e76a54fed67d031cfee6e64e4d3ea
FredoVz <dm.fredo018@gmail.com>
2026-06-07 10:34:48 +0700

    feat: register

--- src/components/views/auth/Register/index.tsx
+++ src/components/views/auth/Register/index.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import styles from "./Register.module.scss";
 import { useRouter } from "next/router";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 const RegisterView = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
   const { push } = useRouter();
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setIsLoading(true);
+    setError("");
     const form = event.target as HTMLFormElement;
     const data = {
       email: form.email.value,
@@ -25,14 +30,17 @@ const RegisterView = () => {
 
     if (result.status === 200) {
       form.reset();
+      setIsLoading(false);
       push("/auth/login");
     } else {
-      console.log("error");
+      setIsLoading(false);
+      setError("Email is already registered");
     }
   };
   return (
     <div className={styles.register}>
       <h1 className={styles.register__title}>Register</h1>
+      {error && <p className={styles.register__error}>{error}</p>}
       <div className={styles.register__form}>
         <form onSubmit={handleSubmit}>
           <div className={styles.register__form__item}>
@@ -63,16 +71,16 @@ const RegisterView = () => {
             />
           </div>
           <div className={styles.register__form__item}>
-            <label htmlFor="passwprd">Password</label>
+            <label htmlFor="password">Password</label>
             <input
-              name="passwprd"
-              id="passwprd"
+              name="password"
+              id="password"
               type="password"
               className={styles.register__form__item__input}
             />
           </div>
           <button type="submit" className={styles.register__form__button}>
-            Register
+            {isLoading ? "Loading..." : "Register"}
           </button>
         </form>
       </div>
