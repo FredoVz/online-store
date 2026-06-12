02c8622eaa3ed9e0660b492b3687cec1262ee7da
FredoVz <dm.fredo018@gmail.com>
2026-06-12 14:33:59 +0700

    feat: update profile

--- src/pages/api/user/profile/[[...user]].ts
+++ src/pages/api/user/profile/[[...user]].ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
 import { retrieveDataById, updateData } from "@/lib/firebase/service";
+import { compare, hash } from "bcrypt";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
@@ -41,6 +42,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const token = req.headers.authorization?.split(" ")[1] || "";
     jwt.verify(token, process.env.NEXTAUTH_SECRET || "", async (err: any, decoded: any) => {
       if (decoded) {
+        if (data.password) {
+          const passwordConfirm = await compare(data.oldPassword, data.encryptedPassword);
+          if (!passwordConfirm) {
+            res.status(400).json({ status: false, statusCode: 400, message: "failed" });
+          }
+          delete data.oldPassword;
+          delete data.encryptedPassword;
+          data.password = await hash(data.password, 10);
+        }
+
         await updateData("users", user[0], data, (result: boolean) => {
           if (result) {
             res.status(200).json({ status: true, statusCode: 200, message: "success" });
