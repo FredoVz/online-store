5e21d8da94cfc50104651704517b3188852475ce
FredoVz <dm.fredo018@gmail.com>
2026-06-14 08:37:25 +0700

    feat: UI Add Product

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -7,15 +7,26 @@ type Proptypes = {
   placeholder?: string;
   defaultValue?: string;
   disabled?: boolean;
+  onChange?: (e: any) => void;
 };
 
 const Input = (props: Proptypes) => {
-  const { label, name, type, placeholder, defaultValue, disabled } = props;
+  const { label, name, type, placeholder, defaultValue, disabled, onChange } =
+    props;
   return (
     <>
       <div className={styles.container}>
         {label && <label htmlFor={name}>{label}</label>}
-        <input name={name} id={name} type={type} placeholder={placeholder} className={styles.container__input} defaultValue={defaultValue} disabled={disabled} />
+        <input
+          name={name}
+          id={name}
+          type={type}
+          placeholder={placeholder}
+          className={styles.container__input}
+          defaultValue={defaultValue}
+          disabled={disabled}
+          onChange={onChange}
+        />
       </div>
     </>
   );
--- src/components/views/admin/Products/index.tsx
+++ src/components/views/admin/Products/index.tsx
@@ -5,6 +5,7 @@ import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import Image from "next/image";
 import { convertIDR } from "@/utils/currency";
 import { Product } from "@/types/product.type";
+import ModalAddProduct from "./ModalAddProduct";
 
 type PropTypes = {
   products: Product[];
@@ -14,6 +15,7 @@ type PropTypes = {
 const ProductsAdminView = (props: PropTypes) => {
   const { products, setToaster } = props;
   const [productsData, setProductsData] = useState<Product[]>([]);
+  const [modalAddProduct, setModalAddProduct] = useState(false);
 
   useEffect(() => {
     setProductsData(products);
@@ -24,6 +26,14 @@ const ProductsAdminView = (props: PropTypes) => {
       <AdminLayout>
         <div className={styles.products}>
           <h1>Product Management</h1>
+          <Button
+            type="button"
+            className={styles.products__add}
+            onClick={() => setModalAddProduct(true)}
+          >
+            <i className="bx bx-plus" />
+            Add Product
+          </Button>
           <table className={styles.products__table}>
             <thead>
               <tr>
@@ -96,6 +106,13 @@ const ProductsAdminView = (props: PropTypes) => {
           </table>
         </div>
       </AdminLayout>
+      {modalAddProduct && (
+        <ModalAddProduct
+          setModalAddProduct={setModalAddProduct}
+          setToaster={setToaster}
+          setProductsData={setProductsData}
+        />
+      )}
     </>
   );
 };
