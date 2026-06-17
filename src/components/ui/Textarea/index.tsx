6b73ff17639b7a4fb68727407bae67b46fe56026
FredoVz <dm.fredo018@gmail.com>
2026-06-17 11:48:05 +0700

    manage address

---
+++ src/components/ui/Textarea/index.tsx
@@ -0,0 +1,29 @@
+import styles from "./Textarea.module.scss";
+
+type Proptypes = {
+  label?: string;
+  name: string;
+  placeholder?: string;
+  defaultValue?: string | number;
+  disabled?: boolean;
+  onChange?: (e: any) => void;
+  className?: string;
+};
+
+const Textarea = (props: Proptypes) => {
+  const { label, name, placeholder, defaultValue, disabled, onChange, className } = props;
+  return (
+    <>
+      <div className={`${styles.container} ${className}`}>
+        {label && (
+          <label className={styles.container__label} htmlFor={name}>
+            {label}
+          </label>
+        )}
+        <textarea name={name} id={name} placeholder={placeholder} className={styles.container__input} defaultValue={defaultValue} disabled={disabled} onChange={onChange} />
+      </div>
+    </>
+  );
+};
+
+export default Textarea;
--- src/components/views/Checkout/index.tsx
+++ src/components/views/Checkout/index.tsx
@@ -19,11 +19,13 @@ const CheckoutView = () => {
   const getProfile = async () => {
     const { data } = await userServices.getProfile();
     setProfile(data.data);
-    data.data.address.filter((address: { isMain: boolean }, id: number) => {
-      if (address.isMain) {
-        setSelectedAddress(id);
-      }
-    });
+    if (data?.data?.address?.length > 0) {
+      data.data.address.filter((address: { isMain: boolean }, id: number) => {
+        if (address.isMain) {
+          setSelectedAddress(id);
+        }
+      });
+    }
   };
 
   const getAllProducts = async () => {
@@ -47,13 +49,10 @@ const CheckoutView = () => {
   };
 
   const getTotalPrice = () => {
-    const total = profile?.carts?.reduce(
-      (acc: number, item: { id: string; size: string; qty: number }) => {
-        const product: any = getProduct(item.id);
-        return (acc += parseInt(product?.price) * item.qty);
-      },
-      0,
-    );
+    const total = profile?.carts?.reduce((acc: number, item: { id: string; size: string; qty: number }) => {
+      const product: any = getProduct(item.id);
+      return (acc += parseInt(product?.price) * item.qty);
+    }, 0);
 
     return total;
   };
@@ -64,91 +63,48 @@ const CheckoutView = () => {
         <div className={styles.checkout__main}>
           <h1 className={styles.checkout__main__title}>Checkout</h1>
           <div className={styles.checkout__main__address}>
-            <h3 className={styles.checkout__main__address__title}>
-              Shipping Address
-            </h3>
+            <h3 className={styles.checkout__main__address__title}>Shipping Address</h3>
             {profile?.address?.length > 0 ? (
               <div className={styles.checkout__main__address__selected}>
                 <h4 className={styles.checkout__main__address__selected__title}>
-                  {profile?.address?.[selectedAddress]?.recipient} -{" "}
-                  {profile?.address?.[selectedAddress]?.phone}
+                  {profile?.address?.[selectedAddress]?.recipient} - {profile?.address?.[selectedAddress]?.phone}
                 </h4>
-                <p
-                  className={styles.checkout__main__address__selected__address}
-                >
-                  {profile?.address?.[selectedAddress]?.addressLine}
-                </p>
-                <p className={styles.checkout__main__address__selected__note}>
-                  Note: {profile?.address?.[selectedAddress]?.note}
-                </p>
+                <p className={styles.checkout__main__address__selected__address}>{profile?.address?.[selectedAddress]?.addressLine}</p>
+                <p className={styles.checkout__main__address__selected__note}>Note: {profile?.address?.[selectedAddress]?.note}</p>
                 <Button type="button" onClick={() => setChangeAddress(true)}>
                   Change Address
                 </Button>
               </div>
             ) : (
-              ""
+              <Button type="button" onClick={() => setChangeAddress(true)}>
+                Add New Address
+              </Button>
             )}
           </div>
           {profile?.carts?.length > 0 ? (
             <div className={styles.checkout__main__list}>
-              {profile?.carts?.map(
-                (item: { id: string; size: string; qty: number }) => (
-                  <Fragment key={`${item.id}-${item.size}`}>
-                    <div className={styles.checkout__main__list__item}>
-                      {getProduct(item.id)?.image && (
-                        <Image
-                          src={`${getProduct(item.id)?.image}`}
-                          width={150}
-                          height={150}
-                          alt={`${item.id}-${item.size}`}
-                          className={styles.checkout__main__list__item__image}
-                        />
-                      )}
-                      <div className={styles.checkout__main__list__item__info}>
-                        <h4
-                          className={
-                            styles.checkout__main__list__item__info__title
-                          }
-                        >
-                          {getProduct(item.id)?.name}
-                        </h4>
+              {profile?.carts?.map((item: { id: string; size: string; qty: number }) => (
+                <Fragment key={`${item.id}-${item.size}`}>
+                  <div className={styles.checkout__main__list__item}>
+                    {getProduct(item.id)?.image && <Image src={`${getProduct(item.id)?.image}`} width={150} height={150} alt={`${item.id}-${item.size}`} className={styles.checkout__main__list__item__image} />}
+                    <div className={styles.checkout__main__list__item__info}>
+                      <h4 className={styles.checkout__main__list__item__info__title}>{getProduct(item.id)?.name}</h4>
 
-                        <div
-                          className={
-                            styles.checkout__main__list__item__info__data
-                          }
-                        >
-                          <label
-                            className={
-                              styles.checkout__main__list__item__info__data__size
-                            }
-                          >
-                            Size {item.size}
-                          </label>
+                      <div className={styles.checkout__main__list__item__info__data}>
+                        <label className={styles.checkout__main__list__item__info__data__size}>Size {item.size}</label>
 
-                          <label
-                            className={
-                              styles.checkout__main__list__item__info__data__qty
-                            }
-                          >
-                            Quantity {item.qty}
-                          </label>
-                        </div>
+                        <label className={styles.checkout__main__list__item__info__data__qty}>Quantity {item.qty}</label>
                       </div>
-                      <h4 className={styles.checkout__main__list__item__price}>
-                        {convertIDR(getProduct(item.id)?.price)}
-                      </h4>
                     </div>
-                    <hr className={styles.checkout__main__list__divider} />
-                  </Fragment>
-                ),
-              )}
+                    <h4 className={styles.checkout__main__list__item__price}>{convertIDR(getProduct(item.id)?.price)}</h4>
+                  </div>
+                  <hr className={styles.checkout__main__list__divider} />
+                </Fragment>
+              ))}
             </div>
           ) : (
             <div className={styles.checkout__main__empty}>
-              <h1 className={styles.checkout__main__empty__title}>
-                Your cart is empty
-              </h1>
+              <h1 className={styles.checkout__main__empty__title}>Your cart is empty</h1>
             </div>
           )}
         </div>
@@ -177,14 +133,7 @@ const CheckoutView = () => {
           </Button>
         </div>
       </div>
-      {changeAddress && (
-        <ModalChangeAddress
-          address={profile.address}
-          setChangeAddress={setChangeAddress}
-          setSelectedAddress={setSelectedAddress}
-          selectedAddress={selectedAddress}
-        />
-      )}
+      {changeAddress && <ModalChangeAddress profile={profile} setProfile={setProfile} setChangeAddress={setChangeAddress} setSelectedAddress={setSelectedAddress} selectedAddress={selectedAddress} />}
     </>
   );
 };
