cf57046e18e74c95837614947769ebc73248ba59
FredoVz <dm.fredo018@gmail.com>
2026-06-19 18:28:22 +0700

    perbaiki skeleton render loading blink

--- src/components/views/Products/CardSkeleton/index.tsx
+++ src/components/views/Products/CardSkeleton/index.tsx
@@ -2,12 +2,16 @@ import styles from "./CardSkeleton.module.scss";
 
 const CardSkeleton = () => {
   return (
-    <div className={styles.cardSkeleton}>
-      <div className={styles.cardSkeleton__image} />
-      <div className={styles.cardSkeleton__title}></div>
-      <div className={styles.cardSkeleton__category}></div>
-      <div className={styles.cardSkeleton__price}></div>
-    </div>
+    <>
+      {Array.from({ length: 5 }).map((_, i) => (
+        <div key={`skeleton-${i}`} className={styles.cardSkeleton}>
+          <div className={styles.cardSkeleton__image} />
+          <div className={styles.cardSkeleton__title}></div>
+          <div className={styles.cardSkeleton__category}></div>
+          <div className={styles.cardSkeleton__price}></div>
+        </div>
+      ))}
+    </>
   );
 };
 
